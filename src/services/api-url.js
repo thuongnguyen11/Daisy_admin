eddbc162c7459d48bd02e59ab86b232a20911133
thuongnguyen11 <hoaithuong2001sm@gmail.com>
2021-12-14 16:49:48 +0700

    Update api url

--- src/services/api-url.js
+++ src/services/api-url.js
@@ -1,2 +1,2 @@
-export const API_URL = ' https://food-app-server-1.herokuapp.com/api/v1/';
+export const API_URL = 'https://daisy-server.herokuapp.com/api/v1/';
 // export const API_URL = ' http://localhost:5000/api/v1/';
