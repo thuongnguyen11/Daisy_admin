c424b8753760f675652b8f723b20c4fd507f57fb
your name <19520856@gm.uit.edu.vn>
2021-12-16 23:34:39 +0700

    https://github.com/kimphung932001/Daisy_admin.git

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -133,7 +133,7 @@ export default function Login() {
                                     <button
                                         disabled={loading}
                                         type="submit"
-                                        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 bg-indigo-100">
+                                        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-800 bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                         {
                                             loading
                                                 ? <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
